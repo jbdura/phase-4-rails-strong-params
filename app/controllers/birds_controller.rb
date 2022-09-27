09a90a89173342afd965dedaaeecc9f33ca27c25
jbdura <bidurajohnson@gmail.com>
2022-09-27 16:08:11 +0300

    commit tree b78ed1ae315e297577e445794d14c7bcb02c07b8

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,4 +1,5 @@
 class BirdsController < ApplicationController
+  wrap_parameters format: []
 
   # GET /birds
   def index
@@ -20,6 +21,41 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  end
+
+  # POST /birds
+
+  #The correct method(function)
+  # def create
+  #   bird = Bird.create(name: params[:name], species: params[:species])
+  #   render json: bird, status: :created
+  # end
+
+  #The risky method(function)
+  # def create
+  #   bird = Bird.create(params)
+  #   render json: bird, status: :created
+  # end
+
+  # #The wrong method(function)
+  # def create
+  #   bird = Bird.create(params.permit(:name, :species))
+  #   render json: bird, status: :created
+  # end
+
+  # POST /birds
+  def create
+    bird = Bird.create(bird_params)
+    render json: bird, status: :created
+  end
+
+  # other controller actions here
+
+  private
+  # all methods below here are private
+
+  def bird_params
+    params.permit(:name, :species)
   end
 
 end
